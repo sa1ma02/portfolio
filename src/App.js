b800d634cf234c8cb6dc4e447dccee7a1b92b229
salma <salmabaalli20@gmai.com>
2024-12-27 20:00:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styled , {ThemeProvider} from 'styled-components';
+import {darkTheme} from './utils/Themes';
+import Skills from './Components/Skills';
+import Hero from './Components/HeroSection';
+import Navbar from './Components/Navbar';
+import Education from './Components/Education';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Experience from './Components/Experience';
+import Projects from './Components/Projects'
+import Footer from './Components/Footer';
+import Contact from './Components/Contact';
+
+
+const Body = styled.div`
+  background-color: ${({theme})=> theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+   `;
+
+   const Wrapper = styled.div`
+   background-color: linear-gradient{
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%,
+   },
+   linear-gradient{
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%,
+   };
+   width: 100%;
+   clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+   `;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+      <Navbar/>
+      <Body>
+      <Hero/>
+      <Wrapper>
+      <Skills/>
+      <Experience/>
+      <Education/>
+      <Projects/>
+      <Contact/>
+      </Wrapper>
+    
+      <Footer/>
+      </Body>
+        </Router>      
+        
+    </ThemeProvider>
   );
 }
 
---
+++ src/data/constants.js
@@ -0,0 +1,326 @@
+import ecommercebg from "../images/ecommercebg.PNG"
+import fleetbg from "../images/fleetbg.PNG"
+import notesbg from "../images/notesbg.PNG"
+import tetrisbg from "../images/tetrisbg.PNG"
+import fbbg from "../images/fbbg.PNG"
+import budgettracker from "../images/budgettracker.PNG"
+import microserv from "../images/microserv.webp"
+
+
+export const Bio = {
+    name: "BAALLI Salma",
+    roles: [
+      "Software Engineering Student",
+      "Full Stack Developer",
+      "Java Programmer"
+      
+    ],
+    description:
+      "I am a motivated and adaptable developer with a passion for both backend and frontend technologies. Specializing in backend development with Java and Spring Boot, I also enjoy crafting seamless user experiences through frontend work. With a growth mindset and a commitment to high-quality results, I strive to make meaningful contributions to every project.",
+    github: "https://github.com/sa1ma02",
+    resume:
+      "https://drive.google.com/file/d/1XhtNxidu3av9-2gf5dYsyAU1Q4zH4zzI/view?usp=sharing",
+    linkedin: "www.linkedin.com/in/salma-baalli",
+    facebook: "/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+    
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Material UI",
+          image: "https://mui.com/static/logo.png"
+        },
+     
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "SpringBoot",
+          image: "https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png",
+        },
+        {
+          name: "Node.js",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png",
+       },
+     
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1200px-C_Programming_Language.svg.png",
+        },
+        {
+          name: "PHP",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png",
+        },
+        {
+          name: "PostgreSQL",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+      
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+       
+      ],
+    },
+
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+     
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image: "https://www.cdnlogo.com/logos/p/20/postman.svg"
+        },
+   
+        {
+          name: "Figma",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1200px-Figma-logo.svg.png",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/OCP_Group.svg/1606px-OCP_Group.svg.png",
+      role: "Fullstack Engineering Intern (PFA)",
+      company: "OCP",
+      date: "July 2024 - August 2024",
+      desc: "Built an auto fleet management full stack web app using React and Spring Boot.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "MySQL",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "Spring Boot",
+        "JavaScript",
+      ],
+    },
+    {
+      id: 0,
+      img: "https://www.rekrute.com/rekrute/file/jobOfferLogo/jobOfferId/35257",
+      role: "Fullstack Engineering Intern (introductory internship)",
+      company: "Tarfaya Energy Company",
+      date: "July 2023",
+      desc: "Digitalization of HSE process",
+      skills: [
+        "MySQL",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "PHP",
+      ],
+      doc: "",
+    },
+
+    
+ 
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/INSEA_logo.png/800px-INSEA_logo.png",
+      school: "National School of statistics and applied economics, Rabat",
+      date: "2022 - 2025",
+      grade: "Data & Software Engineering",
+      desc: "I am currently pursuing a Data & Software Engineering's at National School of statistics and applied economics. I have completed 4 semesters. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. ",
+      degree: "Data & Software Engineering",
+    },
+    {
+      id: 1,
+      img: "https://www.cpge.ac.ma/Commun/Images/logoSC.png",
+      school: "Preparatory Classes for Engineering Schools, Laayoune",
+      date: "2020 - Apr 2022",
+      grade: "Mathematics & Physics",
+      desc: "I studuied in preparatory classes for engineering schools at Laayoune. I have taken courses in Mathematics, Physics, and Computer Science.",
+      degree: "Mathematics and Physics",
+    },
+  
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Expenses Tracker App",
+      date: "March 2024 ",
+      description:
+        "Expenses Tracker is a user-friendly web application designed to help individuals and businesses manage and track their expenses effectively. It allows users to log, categorize, and view their expenses with ease. The app provides detailed reports and analytics to monitor spending habits and stay on top of financial goals. With secure login for both employees and admins, the platform ensures smooth and efficient expense management for organizations and individuals alike",
+      image:
+        budgettracker,
+      tags: [
+        "SpringBoot",
+        "React Js",
+        "PostgreSQL",
+        "Axios",
+      ],
+      category: "web app",
+      github: "https://github.com/sa1ma02/expense-tracker",
+      webapp: "",
+    },
+    {
+      id: 0,
+      title: "Ecommerce application",
+      date: "March 2023 - June 2023",
+      description:
+        "Developed a full-stack ecommerce web application that allows users to browse products, add them to their cart, and complete purchases with ease. Integrated secure user authentication, enabling users to sign up, log in, and manage their profiles. The app features a responsive and intuitive UI built with React JS, ensuring a seamless shopping experience across devices. Utilized Spring Boot for backend services and Node.js for efficient handling of product listings and transactions. Focused on optimizing performance, security, and user experience throughout development",
+      image: ecommercebg,
+      tags: ["React Js", "Springboot", "Node Js", "Redux", "Bootstrap", "Material UI", "JWT"],
+      category: "web app",
+      github: "https://github.com/sa1ma02/e-commerce",
+      webapp: "",
+      member: [
+      
+      ],
+    },
+    {
+      id: 2,
+      title: "Vehicle fleet management",
+      date: "Jul 2024 - Aug 2024",
+      description:
+        "Developed a vehicle fleet management application to track and manage a fleet of vehicles. Implemented features such as vehicle maintenance scheduling, fuel tracking, and real-time monitoring of vehicle status. Built with React for the frontend and Spring Boot for the backend to ensure a scalable and efficient solution",
+      image:
+        fleetbg,
+      tags: ["React Js", "Springboot", "Node Js", "Redux", "Bootstrap", "Material UI", "JWT"],
+      category: "Web app",
+      github: "https://github.com/sa1ma02/gestion-flotte-automobile",
+      webapp: "",
+      member: [
+       
+      ],
+    },
+    {
+      id: 1,
+      title: "Notes app",
+      date: "Dec 2024",
+      description:
+        "Built the Notes app using React JS and Material-UI, allowing users to create, manage, and organize notes. The backend is powered by JSON Server for simple data storage and retrieval. The app provides a smooth, responsive experience with features like note creation, editing, and deletion",
+      image:notesbg,
+      tags: [
+        "React Js",
+        "Mui",
+        "JSON-server"
+       
+      ],
+      category: "web app",
+      github: "https://github.com/sa1ma02/notesApp",
+      webapp: "",
+    },
+ 
+    {
+      id: 3,
+      title: "Tetris Game",
+      date: "Dec 2022 ",
+      description:
+        "Developed a classic Tetris game using pure JavaScript. The game features smooth controls, dynamic level progression, and real-time gameplay, offering an engaging and nostalgic experience for players",
+      image:
+        tetrisbg,
+      tags: ["JavaScript", "HTML"],
+      category: "",
+      github: "https://github.com/sa1ma02/tetrisGame",
+      webapp: "",
+    },
+    {
+      id: 10,
+      title: "Flappy Bird",
+      date: "Apr 2023",
+      description:
+        "Created a Flappy Bird clone using Java, implementing smooth physics, collision detection, and dynamic difficulty. The game features intuitive controls and an engaging, fast-paced experience for players",
+      image:
+        fbbg,
+      tags: ["Java"],
+      category: "",
+      github: "https://github.com/sa1ma02/flappyBird",
+      webapp: "",
+    },
+    {
+      id: 10,
+      title: "Online shop backend using Microservices",
+      date: "Dec 2024",
+      description: "Developed the backend for an online shopping platform using a microservices architecture. The project leveraged technologies like Spring Boot and Docker for service orchestration and scalability. Each microservice was responsible for core functionalities such as user authentication, product management, order processing, and payment integration. Implemented robust API communication using REST, ensured fault tolerance, and optimized the system for seamless deployment and maintenance.",
+
+      image:
+        microserv,
+      tags: ["Spring Boot, Microservices"],
+      category: "",
+      github: "https://github.com/sa1ma02/microservices_onlineShop",
+      webapp: "",
+    },
+    
+   
+
+
+  ];
+  
